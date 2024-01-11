2f811c97cea7f986287f9d3fff580e6818f39d6d
Anurag Shukla <aks76635@gmail.com>
2024-01-11 23:50:07 +0530

    removes unused declarations

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
 const express = require("express");
-const filePath = "./assets/Img.CR3";
+const { imageData } = require("./data");
 const exiftool = require("exiftool-vendored").exiftool;
 
 const app = express();
 app.get("/", (req, res) => res.send("API Running"));
 
+const getExifData = async () => {
+  const responseArray = [];
+  for (const image of imageData) {
+    const tags = await exiftool.read(image.file);
+    const response = {
+      uri: image.uri,
+      info: {
+        Lens: tags.Lens,
+        FocusMode: tags.FocusMode,
+        CaptureTime: tags.DateTimeOriginal.rawValue,
+        ISO: tags.ISO,
+        Speed: tags.ShutterSpeed,
+        Aperture: tags.ApertureValue,
+        FileName: tags.FileName,
+        ImageSize: tags.ImageSize,
+        WhiteBalance: tags.WhiteBalance,
+        Rating: tags.Rating,
+        Camera: tags.CanonImageType,
+      },
+    };
+    responseArray.push(response);
+  }
+
+  return responseArray;
+};
 app.post("/exif", async (req, res) => {
   try {
-    const tags = await exiftool.read(filePath);
-    const response = {
-      Lens: tags.Lens,
-      FocusMode: tags.FocusMode,
-      CaptureTime: tags.DateTimeOriginal.rawValue,
-      ISO: tags.ISO,
-      Speed: tags.ShutterSpeed,
-      Apperture: tags.ApertureValue,
-      FileName: tags.FileName,
-      ImageSize: tags.ImageSize,
-      WhiteBalance: tags.WhiteBalance,
-      Rating: tags.Rating,
-      Camera: tags.CanonImageType,
-    };
-
-    return res.send(response);
+    const responses = await getExifData();
+    return res.json(responses);
   } catch (error) {
     console.error("Something terrible happened:", error);
     return res.status(500).json({ error: "Error processing image metadata" });
