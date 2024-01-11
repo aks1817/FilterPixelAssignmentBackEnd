499118876653cfca036edcb7c3b1e4a155ba2acc
Anurag Shukla <aks76635@gmail.com>
2024-01-11 23:48:16 +0530

    adds iamges to be processed

---
+++ data.js
@@ -0,0 +1,56 @@
+const imageData = [
+  {
+    uri: "https://drive.google.com/file/d/13Q6-CjRfViwHm_MS5TYbZeqH0WOqfijl/view?usp=sharing",
+    file: "./assets/ImageOne.CR3",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1SytTLK-iM4rZzwCFCT_LHVeh-YJ5AgTn/view?usp=sharing",
+    file: "./assets/ImageTwo.CR3",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1SsEMHyPZRleGgLH5FtvemApddcnzjiTM/view?usp=sharing",
+    file: "./assets/ImageThree.PEF",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1e7fzl-qQYw8PgVPM_3sdgFP4gEeT7z7_/view?usp=sharing",
+    file: "./assets/ImageFour.ORF",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1YMncMw3kYLAiHvS3oNyQo5dELeAt9t07/view?usp=sharing",
+    file: "./assets/ImageFive.DCR",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1lXgKbb6FzRlUbTifMFLar6t1mPBcEyR9/view?usp=sharing",
+    file: "./assets/ImageSix.CR3",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1JoCAL7wwJo3gHPx3rPOkm1aTAxZF29Us/view?usp=sharing",
+    file: "./assets/ImageSeven.ARW",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1RCkF7wS0XLDIxvkofQTU4ppKFsmL2cbo/view?usp=sharing",
+    file: "./assets/ImageEight.dng",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1FeDmd84ialK6xHGwel9k7MbkK1eqNrWM/view?usp=sharing",
+    file: "./assets/ImageNine.dng",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1MRWfBVZFiS6Q-1I_0VYeebxe1Hnlbsx2/view?usp=sharing",
+    file: "./assets/ImageTen.dng",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1D2DeBl88DdDW5V2F-wIb8g5K6l2ESnxH/view?usp=sharing",
+    file: "./assets/ImageEleven.NEF",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1Wpy1nat8Kzjyv3xvEsTwy3Vng0zfs7-t/view?usp=sharing",
+    file: "./assets/ImageTwelve.NEF",
+  },
+  {
+    uri: "https://drive.google.com/file/d/1IMzbzF9kNSGBX0JJSWZQSdvX27MnF02_/view?usp=sharing",
+    file: "./assets/ImageThirteen.NEF",
+  },
+];
+
+module.exports = { imageData };
